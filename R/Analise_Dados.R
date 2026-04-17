cc20f591e7956b92f8101e1eaca5d6a9b927c89f
Tecnico012 <Tecnico012@prossenusers.com.br>
2026-04-17 17:41:30 -0300

    V1.4
    - Tratamento não funcional de dados

--- R/Analise_Dados.R
+++ R/Analise_Dados.R
@@ -1,4 +1,4 @@
-install.packages(c("dplyr", "tidyr", "stringr", "lubridate", "janitor","DataExplorer"))
+install.packages(c("dplyr", "tidyr", "stringr", "lubridate", "janitor","DataExplorer","ggplot2"))
 
 library(dplyr)
 library(tidyr)
@@ -6,7 +6,17 @@ library(stringr)
 library(lubridate)
 library(janitor)
 library(DataExplorer)
+library(ggplot2)
 
 df <- readRDS("df_organizado.rds")
-table(df$Projeto)
-plot_missing(df$disciplina,df$responsavel)
+table(df$tempo)
+##plot_missing(df)
+df %>%
+  filter(is.na(disciplina)) %>%
+  count(responsavel) %>%
+  ggplot(aes(x = reorder(responsavel, n), y = n)) +
+  geom_col(fill = "steelblue") +
+  coord_flip() +
+  labs(title = "Quantidade de Disciplinas não preenchidas por Responsável",
+       x = "Responsável",
+       y = "Total de Disciplinas Vazias")
